15d63340a29a2018dfde7ceb005de8b9119d457c
HAEUN <gkdmssidhd@naver.com>
2021-03-30 23:18:32 +0900

    Coding

---
+++ src/level02/Main.java
@@ -0,0 +1,23 @@
+package level02;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int A, B;
+		A = scan.nextInt();
+		B = scan.nextInt();
+		
+		if(A>B) {
+			System.out.println(">");
+		} else if(A<B) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+
+}
