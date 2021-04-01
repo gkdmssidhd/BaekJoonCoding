a783a10653f20be124ee868337245a195f521904
HAEUN <gkdmssidhd@naver.com>
2021-04-01 15:04:31 +0900

    Coding2

--- src/level03/Main02.java
+++ src/level03/Main02.java
@@ -1,5 +1,20 @@
 package level03;
 
-public class Main02 {
+import java.util.Scanner;
 
+public class Main02 {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		
+		scan.close();
+		
+		int sum = 0;
+		for(int i = 1; i<=A; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
 }
