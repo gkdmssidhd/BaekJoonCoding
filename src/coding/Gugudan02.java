42b7aa908eb8088339d5c2c3c540a978420e0df4
gkdmssidhd <75778395+gkdmssidhd@users.noreply.github.com>
2021-07-07 16:38:23 +0900

    07.07

--- src/coding/Gugudan02.java
+++ src/coding/Gugudan02.java
@@ -2,4 +2,16 @@ package coding;
 
 public class Gugudan02 {
 
+	public static void main(String[] args) {
+		
+		for(int i = 2; i <= 8; i += 3) {
+			for(int k = 1; k <= 9; k += 1) {
+				for(int h = i; h <= (i + 2); h += 1) {
+					System.out.print(h + "x" + k +"\t");
+				}
+				System.out.print("\n");
+			}
+			System.out.print("\n");
+		}
+	}
 }
--- src/coding/Gugudan03.java
+++ src/coding/Gugudan03.java
@@ -3,18 +3,12 @@ package coding;
 public class Gugudan03 {
 
 	public static void main(String[] args) {
-		
-		// 한 줄에 출력할 단의 개수 
-		int cnt = 3;
-		
-		for(int i=0; i<2; i++) {
-			for(int y=1; y<=9; y++) {
-				for(int x = 2+cnt*i; x <= 5+cnt*i; x++) {
-					System.out.printf("%d * %d = %d\t", x, y, x*y);
-				}
-				System.out.println();
+	
+		for(int i = 1; i<=3; i ++) {
+			for(int k = 1; k<=9; k++) {
+				System.out.println(i + "*" + k );
 			}
-			System.out.println();
 		}
+		
 	}
 }
