c3e6cf11682c40a5abef19c77d429fc53242aa5a
gkdmssidhd <75778395+gkdmssidhd@users.noreply.github.com>
2021-07-11 20:55:24 +0900

    07.11

---
+++ src/method/MyMath.java
@@ -0,0 +1,20 @@
+package method;
+
+	class MyMath {
+		
+		long add(long a, long b) {
+			long result = a+b;
+			return result;
+			
+		}
+			long subtract(long a, long b) {
+				return a - b; 
+			}
+			long multiply(long a, long b) {
+				return a * b; 
+			}
+			double divide(long a, long b) {
+				return a / b; 
+			}
+		}
+	
