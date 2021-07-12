1017841d8c8666454f47241513f3d9c7b63f619f
gkdmssidhd <75778395+gkdmssidhd@users.noreply.github.com>
2021-07-12 17:55:12 +0900

    07.12 commit

---
+++ src/test/Test01.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		a += 2;
+		System.out.println(a);
+		a %= 5;
+		System.out.println(a);
+	}
+}
---
+++ src/test/Test02.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Test02 {
+	
+	public static void main(String[] args) {
+		
+		int[] exint = {2,4,2,47,6,4,7,2,3,4,5};
+		
+		int value = 0;
+		
+		for(int i = 0; i < exint.length; i++) {
+			if(exint[i] == 2) {
+				value++;
+			}
+		}
+		System.out.println(value);
+	}
+}
